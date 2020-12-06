6d804f28d6e506712fa983cbf32f1a49e3f28091
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2020-12-07 00:58:53 +0900

    [add]geolocation
    gpsで位置取得可能に。実機でも検証済み・

--- ios_app/lib/main.dart
+++ ios_app/lib/main.dart
@@ -5,6 +5,8 @@ import "dart:async";
 import 'chopper_client_creator.dart';
 import 'event_service.dart';
 
+import 'package:geolocator/geolocator.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -55,6 +57,8 @@ class MyHomePage extends StatefulWidget {
 // Home WidgetのState
 class _MyHomePageState extends State<MyHomePage> {
   var _res;
+  Position position;
+
   // API通信
   final EventService service =
       EventService.create(ChopperClientCreator.create());
@@ -94,10 +98,21 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
+  // 位置情報取得
+  Future<void> _getLocation(context) async {
+    Position _currentPosition = await Geolocator().getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high); // ここで精度を「high」に指定している
+    print(_currentPosition);
+    setState(() {
+      position = _currentPosition;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
     _getData();
+    _getLocation(context);
   }
 
   @override
@@ -108,48 +123,83 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hello World',
-            ),
-            Text(
-              '$_res',
-              // '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _updateData,
-        tooltip: 'Update Data',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+    return FutureBuilder<GeolocationStatus>(
+      future: Geolocator().checkGeolocationPermissionStatus(),
+      builder: 
+          (BuildContext context, AsyncSnapshot<GeolocationStatus> snapshot) {
+        if (!snapshot.hasData) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if (snapshot.data == GeolocationStatus.denied) {
+          return Text(
+            'Access to location denied',
+            textAlign: TextAlign.center,
+          );
+        }
+
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Location Infomation",
+                style: TextStyle(
+                  fontSize: 20.0
+                ),
+              ),
+              Text("Your Current Location is :"),
+              Text("${position}")
+            ],
+          ),
+        );
+      }
     );
+
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     // Here we take the value from the MyHomePage object that was created by
+    //     // the App.build method, and use it to set our appbar title.
+    //     title: Text(widget.title),
+    //   ),
+    //   body: Center(
+    //     // Center is a layout widget. It takes a single child and positions it
+    //     // in the middle of the parent.
+    //     child: Column(
+    //       // Column is also a layout widget. It takes a list of children and
+    //       // arranges them vertically. By default, it sizes itself to fit its
+    //       // children horizontally, and tries to be as tall as its parent.
+    //       //
+    //       // Invoke "debug painting" (press "p" in the console, choose the
+    //       // "Toggle Debug Paint" action from the Flutter Inspector in Android
+    //       // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+    //       // to see the wireframe for each widget.
+    //       //
+    //       // Column has various properties to control how it sizes itself and
+    //       // how it positions its children. Here we use mainAxisAlignment to
+    //       // center the children vertically; the main axis here is the vertical
+    //       // axis because Columns are vertical (the cross axis would be
+    //       // horizontal).
+    //       mainAxisAlignment: MainAxisAlignment.center,
+    //       children: <Widget>[
+    //         Text(
+    //           'Hello World',
+    //         ),
+    //         Text(
+    //           '$_res',
+    //           // '$_counter',
+    //           style: Theme.of(context).textTheme.headline4,
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    //   floatingActionButton: FloatingActionButton(
+    //     onPressed: _updateData,
+    //     tooltip: 'Update Data',
+    //     child: Icon(Icons.add),
+    //   ), // This trailing comma makes auto-formatting nicer for build methods.
+    // );
   }
 }
