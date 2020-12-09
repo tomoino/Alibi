7cbb4a20fbb5ab3a21047d2493e6c624d48759e3
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2020-12-09 16:14:18 +0900

    [add]GPS
    GPS情報の取得

--- swift_app/Alibi/Views/ContentView.swift
+++ swift_app/Alibi/Views/ContentView.swift
@@ -6,16 +6,28 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
     // ObservableObject に準拠したクラスを監視
     @ObservedObject var apiClient = ApiClient()
+    @ObservedObject var locationManager = LocationManager()
+    
+    var userLatitude: String {
+        return "\(locationManager.lastLocation?.coordinate.latitude ?? 0)"
+    }
+
+    var userLongitude: String {
+        return "\(locationManager.lastLocation?.coordinate.longitude ?? 0)"
+    }
 
     init(){
         apiClient.getAllEvents()
     }
     
     var body: some View {
+        HStack {
+            Text("latitude: \(userLatitude)")
+            Text("longitude: \(userLongitude)")
+        }
         NavigationView {
             // 通信クラスの eventData プロパティを設定
             List(apiClient.eventData) { event in
