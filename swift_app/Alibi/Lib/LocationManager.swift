51c694bb5e808ef0f9e224c2c252d589f762a73d
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2020-12-11 02:12:35 +0900

    [add]beacon
    beacon1個からの強度取得

--- swift_app/Alibi/Lib/LocationManager.swift
+++ swift_app/Alibi/Lib/LocationManager.swift
@@ -11,7 +11,11 @@ import Combine
 
 class LocationManager: NSObject, ObservableObject {
 
+    var beaconRegion : CLBeaconRegion!
+    var beacon1: Int
+
     override init() {
+        self.beacon1 = -100
         super.init()
         self.locationManager.delegate = self
         self.locationManager.allowsBackgroundLocationUpdates = true; // バックグランドモードで使用する場合YESにする必要がある
@@ -19,6 +23,13 @@ class LocationManager: NSObject, ObservableObject {
         self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
         self.locationManager.requestWhenInUseAuthorization()
         self.locationManager.startUpdatingLocation()
+        
+//        var trackLocationManager : CLLocationManager!
+                // BeaconのUUIDを設定
+        let uuid:UUID? = UUID(uuidString: "FDA50693-A4E2-4FB1-AFCF-C6EB07647825")
+
+        //Beacon領域を作成
+        self.beaconRegion = CLBeaconRegion(proximityUUID: uuid!, identifier: "net.noumenon-th")
         
         // 位置情報の認証チェック
         let status = CLLocationManager.authorizationStatus()
@@ -78,15 +89,68 @@ class LocationManager: NSObject, ObservableObject {
 
 extension LocationManager: CLLocationManagerDelegate {
 
+    //位置認証のステータスが変更された時に呼ばれる
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         self.locationStatus = status
         print(#function, statusString)
+        manager.startMonitoring(for: self.beaconRegion)
+    }
+    
+    //観測の開始に成功すると呼ばれる
+    func locationManager(_ manager: CLLocationManager, didStartMonitoringFor region: CLRegion) {
+        //観測開始に成功したら、領域内にいるかどうかの判定をおこなう。→（didDetermineState）へ
+        manager.requestState(for: self.beaconRegion)
+    }
+    
+    //領域内にいるかどうかを判定する
+    func locationManager(_ manager: CLLocationManager, didDetermineState state: CLRegionState, for inRegion: CLRegion) {
+        switch (state) {
+            case .inside: // すでに領域内にいる場合は（didEnterRegion）は呼ばれない
+                manager.startRangingBeacons(in: beaconRegion)
+                // →(didRangeBeacons)で測定をはじめる
+                break
+
+            case .outside:
+                // 領域外→領域に入った場合はdidEnterRegionが呼ばれる
+                break
+
+            case .unknown:
+                // 不明→領域に入った場合はdidEnterRegionが呼ばれる
+                break
+
+        }
+    }
+    
+    //領域に入った時
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+            // →(didRangeBeacons)で測定をはじめる
+            manager.startRangingBeacons(in: self.beaconRegion)
     }
 
+    //領域から出た時
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        //測定を停止する
+        manager.stopRangingBeacons(in: self.beaconRegion)
+    }
+    
+    //領域内にいるので測定をする
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion){
+            if(beacons.count > 0){
+                for i in 0 ..< beacons.count {
+                    let beacon = beacons[i]
+                    let beaconUUID = beacon.proximityUUID;
+                    let rssi = beacon.rssi;
+                    print("UUID:\(beacon.proximityUUID), rssi: \(beacon.rssi)")
+                    self.beacon1 = beacon.rssi
+                }
+            }
+    }
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else { return }
         self.lastLocation = location
         print(#function, location)
+        print("Location: \(self.beacon1)")
         
         let date = Date()
         let calendar = Calendar.current
@@ -110,5 +174,4 @@ extension LocationManager: CLLocationManagerDelegate {
             apiClient.createEvent(event: event)
         }
     }
-
 }
