447c7033e44c8c3d4deff02a077f3e65fdb11b81
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2020-12-20 13:51:04 +0900

    [update]kuromoji
    kuromojiによる形態素解析の導入

--- node_chrome_extension/app/scripts/contentscript.ts
+++ node_chrome_extension/app/scripts/contentscript.ts
@@ -8,3 +8,21 @@ var textList = document.body.innerText.split('\n')
 textList = textList.filter(text => text.replace(" ", "").replace("　", ""))
 var res = textList.join('\n')
 console.log(res)
+
+import * as kuromoji from 'kuromoji';
+
+console.log(chrome.extension.getURL("resources/dict"))
+
+const builder = kuromoji.builder({
+    dicPath: chrome.extension.getURL("resources/dict")
+})
+  
+builder.build((err, tokenizer) => {
+    if (err) return;
+
+    var tokens = tokenizer.tokenize(res);
+    console.dir(tokens);
+    for (var token in tokens) {
+        console.log(tokens[token].basic_form)
+    }
+})
