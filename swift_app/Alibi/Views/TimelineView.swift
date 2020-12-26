543018f5b7c5bd922e2189402e7209212ab1540a
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2020-12-27 02:39:07 +0900

    [update]timeline
    イベントカードの実装

--- swift_app/Alibi/Views/TimelineView.swift
+++ swift_app/Alibi/Views/TimelineView.swift
@@ -41,18 +41,55 @@ struct DayTimeline: View {
     var body: some View {
         Text("12月\(day)日").font(.title)
         ScrollView(.vertical) {
-                    VStack(spacing: 10) {
+            ZStack {
+                // 時間軸
+                VStack(spacing: 10) {
+                    Divider()
+                    ForEach(0..<24) {
+                        Text("\($0):00")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(EdgeInsets(top: 2, leading: 12, bottom: 10, trailing: 12))
                         Divider()
-                        ForEach(0..<24) {
-                            Text("\($0):00")
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .padding(EdgeInsets(top: 2, leading: 12, bottom: 10, trailing: 12))
-                            Divider()
-                        }
-                        Spacer(minLength: 50)   // Viewの最下段にスペースを追加
                     }
-                    .frame(maxWidth: .infinity) // スクロールの対象範囲を画面幅いっぱいにする為
+                    Spacer(minLength: 50)   // Viewの最下段にスペースを追加
                 }
+                .frame(maxWidth: .infinity) // スクロールの対象範囲を画面幅いっぱいにする為
+                
+                EventCard(event: "プロ研", hour: 5, min: 0, length: 60)
+                EventCard(event: "回路理論", hour: 7, min: 0, length: 120)
+//                    .padding(EdgeInsets(top: 2, leading: 60, bottom: 10, trailing: 12))
+            }
+        }
+    }
+}
+
+struct EventCard: View {
+    var event: String
+    var hour: Int
+    var min: Int
+    var length: Int
+    
+    var body: some View {
+        HStack(alignment: .center) {
+            Text(event)
+            .font(.system(size: 22, weight: .bold, design: .default))
+            .foregroundColor(.white)
+                .frame(height: 52*CGFloat(length/60))
+            .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 10))
+            Spacer()
+        }
+        .frame(maxWidth: .infinity, alignment: .top)
+        .background(Color(red: 32/255, green: 36/255, blue: 38/255))
+        .modifier(CardModifier())
+        .padding(EdgeInsets(top: CGFloat(-662 + 52 * (hour + min/60)), leading: 70, bottom: 0, trailing: 12))
+    }
+}
+
+struct CardModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .cornerRadius(20)
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 0)
     }
 }
 
