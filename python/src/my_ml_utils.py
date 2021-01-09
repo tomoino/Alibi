e2df1215a9e6111962d8620cff50db1d7f22f529
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2021-01-09 15:49:47 +0900

    [update]cnn
    train cnnをリファクタ

--- python/src/my_ml_utils.py
+++ python/src/my_ml_utils.py
@@ -1,5 +1,5 @@
 #coding: utf-8
-# 2層CNNによる実装
+# 機械学習で使える関数等をまとめておく
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras as keras
@@ -22,14 +22,13 @@ import itertools
 from matplotlib import rcParams
 import matplotlib
 
-import my_ml_utils as ml
-
 # matplotlib 日本語対応
 rcParams['font.family'] = ['Noto Sans CJK JP']
 matplotlib.font_manager._rebuild()
 
 # データの不均衡性への対策
 from imblearn.keras import balanced_batch_generator
+import pickle
 
 gpu_id = 0
 physical_devices = tf.config.list_physical_devices('GPU')
@@ -38,11 +37,13 @@ tf.config.set_visible_devices(physical_devices[gpu_id], 'GPU')
 tf.config.experimental.set_memory_growth(physical_devices[gpu_id], True)
 
 # parameter
+MODEL_NAME = "2L_CNN_ENSEMBLE"
 MAX_LENGTH = 3000
-EPOCH = 200
+EPOCH = 1
 BATCH_SIZE = 32
 CATEGORIES = ["プロ研", "回路理論", "多変量解析", "ビジネス", "電生実験", "OS", "論文読み", "開発環境構築"]
 # CATEGORIES = ["プロ研", "回路理論", "多変量解析", "ビジネス", "電生実験", "OS", "論文読み", "開発環境構築", "語学"]
+MODEL_NUM = 5 # アンサンブルに使うモデルの数
 
 category_dict = {}
 # category_dict = {"プロ研": 0, "回路理論": 1, "多変量解析": 2, "ビジネス":3, "電生実験": 4, "OS": 5, "論文読み": 6, "開発環境構築": 7, "語学": 8}
@@ -138,131 +139,88 @@ def plot_confusion_matrix(cmx, classes, metrics_dir, normalize=False, title='Con
     plt.show()
 
 # モデルの可視化
-def visualize_model(model, save_path):
+def visualize_model(model, result_dir):
+    save_path = f"{result_dir}/model.png"
     plot_model(model, to_file=save_path)
 
-def train(_train_data, test_data, embedding_matrix, batch_size=BATCH_SIZE, epoch_count=100, max_length=MAX_LENGTH, model_name="model", learning_rate=0.001):
-    random.shuffle(_train_data)
-    model_filepath = f"../model/model_{model_name}.h5"
-    result_dir = f"../result/{model_name}"
+# 学習で共通して使うデータをpickleにして保存する
+def save_dataset():
+    embedding_matrix, word_index = load_word_vec("../data/w2v.txt")
+    data = load_data("../data/documents.csv", word_index)
 
+    test_data = []
     train_data = []
-    validation_data = []
 
-    validation_data_rate = 0.25  # validationの割合
+    test_data_rate = 0.2 # testの割合
 
     categorized_data = {category_dict[category]: [] for category in CATEGORIES}  # カテゴリごとに分けられたデータ
-    for target_value, input_value in _train_data:
+    for target_value, input_value in data:
         categorized_data[target_value].append((target_value, input_value))
 
     # train, validation, test データのカテゴリ組成を等しくする
     for category_id in categorized_data:
         data_len = len(categorized_data[category_id])
-        validation_boundary = int(data_len * validation_data_rate)
+        test_boundary = int(data_len * test_data_rate)
 
-        validation_data += categorized_data[category_id][0:validation_boundary]
-        train_data += categorized_data[category_id][validation_boundary:]
+        test_data += categorized_data[category_id][0:test_boundary]
+        train_data += categorized_data[category_id][test_boundary:]
     
-    test_inputs = []
-    test_targets = []
-    train_inputs = []
-    train_targets = []
-    validation_inputs = []
-    validation_targets = []
+    with open(f'../data/embedding_matrix.pickle', 'wb') as f:
+        pickle.dump(embedding_matrix, f)
 
-    for target_value, input_value in test_data:
-        test_inputs.append(input_value)
-        test_targets.append([1 if i == target_value else 0 for i in range(len(category_dict))]) # 正解ラベルだけ1にした配列
-
-    for target_value, input_value in validation_data:
-        validation_inputs.append(input_value)
-        validation_targets.append([1 if i == target_value else 0 for i in range(len(category_dict))]) # 正解ラベルだけ1にした配列
-
-    for target_value, input_value in train_data:
-        train_inputs.append(input_value)
-        train_targets.append([1 if i == target_value else 0 for i in range(len(category_dict))]) # 正解ラベルだけ1にした配列
-
-    # カテゴリごとに含まれる数を表示
-    count_by_categories = {key: np.concatenate([np.argmax(test_targets, 1), np.argmax(validation_targets, 1), np.argmax(train_targets, 1)] ).tolist().count(category_dict[key]) for key in CATEGORIES}
-    test_count_by_categories = {key: np.argmax(test_targets, 1).tolist().count(category_dict[key]) for key in CATEGORIES}
-    validation_count_by_categories = {key: np.argmax(validation_targets, 1).tolist().count(category_dict[key]) for key in CATEGORIES}
-    train_count_by_categories = {key: np.argmax(train_targets, 1).tolist().count(category_dict[key]) for key in CATEGORIES}
-    print("ALL: ", count_by_categories)
-    print("TRAIN: ", train_count_by_categories)
-    print("VALIDATION: ", validation_count_by_categories)
-    print("TEST: ", test_count_by_categories)
-
-    test_inputs = np.array(test_inputs)
-    test_targets = np.array(test_targets)
-    train_inputs = np.array(train_inputs)
-    train_targets = np.array(train_targets)
-    validation_inputs = np.array(validation_inputs)
-    validation_targets = np.array(validation_targets)
+    with open(f'../data/word_index.pickle', 'wb') as f:
+        pickle.dump(word_index, f)
     
-    # 単語数、embeddingの次元
-    num_words, word_vec_size = embedding_matrix.shape
-    # モデルの構築
-    model = Sequential([
-        Embedding(num_words, word_vec_size,
-                weights=[embedding_matrix], 
-                input_length=max_length,
-                trainable=False, 
-                mask_zero=True),
-        Conv1D(16, 5, activation='relu'),
-        AveragePooling1D(7),
-        Dropout(0.5),
-        Conv1D(16, 5, activation='relu'),
-        GlobalAveragePooling1D(),
-        Dense(128, activation='relu'),
-        Dropout(0.3),
-        Dense(len(CATEGORIES), activation='softmax')
-    ])
+    with open(f'../data/test_data.pickle', 'wb') as f:
+        pickle.dump(test_data, f)
 
-    model.summary()
+    with open(f'../data/train_data.pickle', 'wb') as f:
+        pickle.dump(train_data, f)
 
-    #Embedding層は学習しないようする
-    model.layers[0].trainable = False
+def load_dataset():
+    with open(f'../data/embedding_matrix.pickle', 'rb') as f:
+        embedding_matrix = pickle.load(f)
 
-    # model.compile(loss='categorical_crossentropy',
-    #           optimizer=keras.optimizers.Adam(1e-4),
-    #           metrics=['accuracy'])
-    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    # with open(f'../data/word_index.pickle', 'rb') as f:
+    #     word_index = pickle.load(f)
+    
+    with open(f'../data/test_data.pickle', 'rb') as f:
+        test_data = pickle.load(f)
 
-    # checkpointの設定
-    checkpoint = ModelCheckpoint(
-        filepath=model_filepath,
-        monitor='val_loss',
-        save_best_only=True,
-        period=1,
-    )
+    with open(f'../data/train_data.pickle', 'rb') as f:
+        train_data = pickle.load(f)
 
-    # データの不均衡性への対策
-    training_generator, steps_per_epoch = balanced_batch_generator(
-        train_inputs, train_targets, batch_size=8, random_state=42)
-    validation_generator, validation_steps = balanced_batch_generator(
-        validation_inputs, validation_targets, batch_size=8, random_state=42)
+    return embedding_matrix, train_data, test_data
 
-    # 学習
-    history = model.fit_generator(generator=training_generator,
-            steps_per_epoch=steps_per_epoch,
-            epochs=epoch_count,
-            verbose=1,
-            validation_data=validation_generator,
-            validation_steps=validation_steps,
-            callbacks=[checkpoint],
-            shuffle=True)
+def model_evaluate(model, test_inputs, test_targets):
+    score = model.evaluate(test_inputs, test_targets, verbose=0)
+    print('Test on '+str(len(test_inputs))+' examples')
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
 
-    # 最良の結果を残したモデルを読み込む
-    model = load_model(model_filepath)
+def classification_evaluate(model, test_inputs, test_targets, result_dir):
+    predict_classes = model.predict_classes(test_inputs)
+    true_classes = np.argmax(test_targets, 1)
 
-    ml.model_evaluate(model, test_inputs, test_targets)
-    ml.classification_evaluate(model, test_inputs, test_targets, result_dir)
-    ml.visualize_model(model, result_dir)
-    ml.save_history(history, result_dir)
+    print(classification_report(true_classes, predict_classes, labels=list(range(0, len(CATEGORIES))), target_names=CATEGORIES))
+    cmx = confusion_matrix(true_classes, predict_classes)
+    plot_confusion_matrix(cmx=cmx, classes=CATEGORIES, metrics_dir=result_dir, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues)
 
-def main(model_name):
-    embedding_matrix, train_data, test_data = ml.load_dataset()
-    train(train_data, test_data, embedding_matrix, epoch_count=EPOCH, model_name=model_name)
+def save_history(history, result_dir):
+    history_df = pd.DataFrame(history.history)
 
-if __name__ == "__main__":
-    main("2L_CNN")
+    history_df.loc[:,['val_loss','loss']].plot()
+    plt.savefig(f"{result_dir}/loss.png")
+    history_df.loc[:,['val_accuracy','accuracy']].plot()
+    plt.savefig(f"{result_dir}/accuracy.png")
+
+def ensemble_predict_classes(models, inputs):
+	preds = [model.predict(inputs) for model in models]
+	preds = np.array(preds)
+
+	# sum across ensemble members
+	summed = np.sum(preds, axis=0)
+	# argmax across classes
+	result = argmax(preds, axis=1)
+
+	return result
--- python/src/train_ensemble.sh
+++ python/src/train_ensemble.sh
@@ -1,1 +1,2 @@
-python -c "import train_2L_cnn_ensemble as tr; tr.save_dataset(); exit();"
+# python -c "import my_ml_utils as ml; ml.save_dataset(); exit();"
+python -c "import train_2L_cnn as cnn; cnn.main('2L_CNN_ENSEMBLE_1'); exit();"
